aa5944f280d22b0b8151969c87ed8a0e7d0425cf
fmapilis <fmapilis@gmail.com>
2023-08-21 11:50:29 -0700

    feat: add text length validation to note form

--- components/TextEditor/SaveButton.tsx
+++ components/TextEditor/SaveButton.tsx
@@ -14,23 +14,44 @@ const SaveButton = ({
   onSave: (title: string, content: string) => Promise<void>;
 }) => {
   const [loading, setLoading] = useState(false);
-  const { getMarkdown } = useHelpers();
+  const { getMarkdown, getText } = useHelpers();
 
   const handleClick = async () => {
     setLoading(true);
     try {
+      // `markdown` includes all formatting, while `text` is just the plain text
+      // This is useful for validation, since we don't want to count the markup
+      // against their total text length.
       const markdown = getMarkdown();
+      const text = getText();
 
-      if (!title?.trim()) {
-        alert("Please enter a title for your note");
-        setLoading(false);
-        return;
-      } else if (!markdown?.trim()) {
-        alert("Please enter some content for your note");
-        setLoading(false);
-        return;
+      const trimmedTitle = (title || "").trim();
+      const trimmedContent = (text || "").trim();
+
+      const validationErrors = [];
+
+      if (!trimmedTitle) {
+        validationErrors.push("Please enter a title for your note");
+      } else if (trimmedTitle.length > 100) {
+        validationErrors.push(
+          "Please enter a title that is less than 100 characters"
+        );
       }
-      await onSave(title, markdown);
+
+      if (trimmedContent.length < 20) {
+        validationErrors.push("Please enter some more content for your note");
+      } else if (trimmedContent.length > 300) {
+        validationErrors.push(
+          "Maximum note length is 300 characters, please shorten your note"
+        );
+      }
+
+      if (validationErrors.length) {
+        alert(validationErrors.join("\n"));
+      } else {
+        await onSave(title as string, markdown);
+      }
+
       setLoading(false);
     } catch (e) {
       alert("There was an unexpected issue saving your note");
